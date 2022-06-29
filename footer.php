5941ae0955c6c5cdc49b9394dfe8b7b745b0a883
unknown <alexandreroffiaen@gmail.com>
2022-06-29 15:57:25 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,5 +1,20 @@
-    <footer class="pied de page">
-
+    <footer class="pied-de-page">
+        <div>
+            <a href="https://ar-web-ouest.fr">
+                <img src="img/cropped-Logo-Alex.png" alt="logo ar-web-ouest.fr">
+                <figcaption>
+                    AR-WEB-OUEST.FR
+                </figcaption>
+            </a>
+        </div>
+        <nav>
+            <ul>
+                <li><a class="btn" href="index.php">accueil</a></li>
+                <li><a class="btn" href="#">hotel</a></li>
+                <li><a class="btn" href="contact.php">contact</a></li>
+                <li><a class="btn" href="#">à propos</a></li>
+            </ul>
+        </nav>
     </footer>
 </body>
 </html>
